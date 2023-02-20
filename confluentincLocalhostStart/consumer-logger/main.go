6b73373841820f55ef6a1792c6bc50d142ab4933
Jerome Bidault <djedje.thai.ok@gmail.com>
2023-02-20 19:34:26 +0700

    ok

--- confluentincLocalhostStart/consumer-logger/main.go
+++ confluentincLocalhostStart/consumer-logger/main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"os"
 	"time"
 
 	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
 )
 
 func main() {
+
+	createTopic()
 
 	c, err := kafka.NewConsumer(&kafka.ConfigMap{
 		"bootstrap.servers": "localhost:29092",
@@ -37,4 +41,45 @@ func main() {
 	}
 
 	c.Close()
+}
+
+func createTopic() {
+	// admin create a topic
+	a, err := kafka.NewAdminClient(&kafka.ConfigMap{"bootstrap.servers": "localhost:29092"})
+	if err != nil {
+		fmt.Printf("Failed to create Admin client: %s\n", err)
+		os.Exit(1)
+	}
+	defer a.Close()
+
+	// Contexts are used to abort or limit the amount of time
+	// the Admin call blocks waiting for a result.
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// Create topics on cluster.
+	// Set Admin options to wait for the operation to finish (or at most 60s)
+	maxDur, err := time.ParseDuration("60s")
+	if err != nil {
+		panic("ParseDuration(60s)")
+	}
+	results, err := a.CreateTopics(
+		ctx,
+		// Multiple topics can be created simultaneously
+		// by providing more TopicSpecification structs here.
+		[]kafka.TopicSpecification{{
+			Topic:             "myTopic",
+			NumPartitions:     4,
+			ReplicationFactor: 1}},
+		// Admin options
+		kafka.SetAdminOperationTimeout(maxDur))
+	if err != nil {
+		fmt.Printf("Failed to create topic: %v\n", err)
+		os.Exit(1)
+	}
+
+	// Print results
+	for _, result := range results {
+		fmt.Printf("%s\n", result)
+	}
 }
