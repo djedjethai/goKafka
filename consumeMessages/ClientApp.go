c9475fd1df60b73e564081b5a0b907e483f50279
Jerome Bidault <djedje.thai.ok@gmail.com>
2023-05-12 16:03:48 +0700

    commit offsets manually, and handle commits when re-balancing

--- consumeMessages/ClientApp.go
+++ consumeMessages/ClientApp.go
@@ -8,24 +8,32 @@ import (
 	"log"
 	"math/rand"
 	"os"
+	"os/signal"
 	"strings"
-	"sync"
+	// "sync"
+	"syscall"
 	"time"
 
 	pb "getting-started-with-ccloud-golang/api/v1/proto"
 	"github.com/golang/protobuf/proto"
 	"github.com/riferrei/srclient"
 	"gopkg.in/confluentinc/confluent-kafka-go.v1/kafka"
-	// "github.com/confluentinc/confluent-kafka-go/kafka"
+	// "github.com/confluentinc/confluent-kafka-go/v2/kafka"
 )
 
 const (
-	producerMode string = "producer"
-	consumerMode string = "consumer"
-	schemaFile   string = "./api/v1/proto/Person.proto"
+	producerMode    string = "producer"
+	consumerMode    string = "consumer"
+	schemaFile      string = "./api/v1/proto/Person.proto"
+	commitBatchSize int    = 5
 	// schemaFile   string = "./api/v1/proto/SensorReading.proto"
 	// messageFile  string = "./api/v1/proto/Message.proto"
 )
+
+// type Event interface {
+// 	// String returns a human-readable representation of the event
+// 	String() string
+// }
 
 var devices = []*SensorReading_Device{
 	{
@@ -109,7 +117,7 @@ func producer(topic string) {
 
 	schema, err := schemaRegistryClient.GetLatestSchema(topic)
 
-	fmt.Println("The schema: ", schema)
+	// fmt.Println("The schema: ", schema)
 
 	// // !!! if I need to update the shema version => recreate the schema
 	// // what ever the changement in the schema, at the time it's recreated
@@ -182,9 +190,9 @@ func producer(topic string) {
 		valueBytes, _ := proto.Marshal(&msg)
 		recordValue = append(recordValue, valueBytes...)
 
-		fmt.Println("see topic: ", topic)
-		fmt.Println("see key: ", key)
-		fmt.Println("see value: ", string(recordValue))
+		// fmt.Println("see topic: ", topic)
+		// fmt.Println("see key: ", key)
+		// fmt.Println("see value: ", string(recordValue))
 
 		producer.Produce(&kafka.Message{
 			TopicPartition: kafka.TopicPartition{
@@ -199,129 +207,6 @@ func producer(topic string) {
 /**************************************************/
 /******************** Consumer ********************/
 /**************************************************/
-
-// type StickyAssignor struct {
-// 	consumer *kafka.Consumer
-// }
-//
-// func (a *StickyAssignor) Assign(
-// 	partitions []kafka.TopicPartition,
-// 	members []string,
-// ) (map[string][]kafka.TopicPartition, error) {
-// 	assignments := make(map[string][]kafka.TopicPartition)
-// 	memberCount := len(members)
-//
-// 	// Assign partitions in a sticky manner
-// 	for i, partition := range partitions {
-// 		memberIndex := i % memberCount
-// 		memberID := members[memberIndex]
-// 		assignments[memberID] = append(assignments[memberID], partition)
-// 	}
-//
-// 	return assignments, nil
-// }
-//
-// func (a *StickyAssignor) OnRevocation(revokedPartitions []kafka.TopicPartition) {
-// 	// Handle logic when partitions are revoked from consumers
-// 	for _, revokedPartition := range revokedPartitions {
-// 		// Commit the current offset for revoked partitions
-// 		a.consumer.CommitOffsets([]kafka.TopicPartition{revokedPartition})
-//
-// 		// Seek to the new position for revoked partitions (end offset)
-// 		tp := kafka.TopicPartition{
-// 			Topic:     revokedPartition.Topic,
-// 			Partition: revokedPartition.Partition,
-// 			Offset:    kafka.OffsetEnd,
-// 		}
-//
-// 		_, high, err := a.consumer.QueryWatermarkOffsets(*tp.Topic, tp.Partition, 5000)
-// 		if err != nil {
-// 			// Handle error
-// 			continue
-// 		}
-//
-// 		tp.Offset = kafka.Offset(high)
-//
-// 		a.consumer.Seek(tp, 0)
-//
-// 		// Perform any other necessary actions for revoked partitions
-// 	}
-// }
-
-type StickyAssignor struct {
-	consumer           *kafka.Consumer
-	assignedPartitions map[int32]bool
-}
-
-func NewStickyAssignor(consumer *kafka.Consumer) *StickyAssignor {
-	return &StickyAssignor{
-		consumer:           consumer,
-		assignedPartitions: make(map[int32]bool),
-	}
-}
-
-func (s *StickyAssignor) AssignPartitions(topic string, partitions []kafka.TopicPartition) {
-	currentAssignment, err := s.consumer.Assignment()
-	log.Println("See the current assignement: ", currentAssignment)
-	if err != nil {
-		panic(err)
-	}
-
-	// Find the partitions that are already assigned to the consumer
-	// assignedPartitions := make(map[int32]bool)
-	for _, p := range currentAssignment {
-		if *p.Topic == topic {
-			s.assignedPartitions[p.Partition] = true
-		}
-	}
-
-	// Assign the unassigned partitions
-	var newAssignment []kafka.TopicPartition
-	for _, p := range partitions {
-		if !s.assignedPartitions[p.Partition] {
-			newAssignment = append(newAssignment, p)
-			// s.assignedPartitions[p.Partition] = true
-		}
-	}
-
-	// Manually assign the unassigned partitions
-	err = s.consumer.Assign(newAssignment)
-	if err != nil {
-		panic(err)
-	}
-	// update the s.assignedPartitions
-	for _, p := range newAssignment {
-		s.assignedPartitions[p.Partition] = true
-	}
-
-	fmt.Println("Assigned partitions:", newAssignment)
-}
-
-func (s *StickyAssignor) OnRebalance(partitions []kafka.TopicPartition) error {
-	fmt.Printf("Rebalance event: %v\n", partitions)
-
-	// Iterate through partitions and reassign the desired ones
-	for _, partition := range partitions {
-		if s.assignedPartitions[partition.Partition] {
-			fmt.Printf("Reassigning partition %d\n", partition.Partition)
-			err := s.consumer.Assign([]kafka.TopicPartition{
-				kafka.TopicPartition{
-					// Topic:     &partition.Topic,
-					Topic:     partition.Topic,
-					Partition: partition.Partition,
-					Offset:    kafka.OffsetInvalid,
-				},
-			})
-
-			if err != nil {
-				fmt.Printf("Error reassigning partition %d: %v\n", partition.Partition, err)
-				return err
-			}
-		}
-	}
-
-	return nil
-}
 
 // func consumer(props map[string]string, topic string) {
 func consumer(topic string) {
@@ -347,124 +232,106 @@ func consumer(topic string) {
 	}
 	defer c.Close()
 
-	// assignor := NewStickyAssignor(c)
-
-	// partitions := []kafka.TopicPartition{
-	// 	{Topic: &topic, Partition: 0},
-	// 	{Topic: &topic, Partition: 1},
-	// 	{Topic: &topic, Partition: 2},
-	// 	{Topic: &topic, Partition: 3},
-	// 	// Add more partitions as needed
-	// }
-
-	// assignor.AssignPartitions(topic, partitions)
-
-	// c.SetRebalanceCallback(onRebalance)
-
-	fmt.Println("bf the subscribe topic: ", topic)
-
-	c.SubscribeTopics([]string{topic}, nil)
+	c.SubscribeTopics([]string{topic}, myRebalanceCallback)
 
 	run := true
 
-	fmt.Println("bf the loop")
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
 
-	// as there is no commitSync() in this library,
-	// use sync.WaitGroup to use the async CommitMessage() as synchronous commit
-	var wg sync.WaitGroup
-	var record *kafka.Message
-	var count int32
+	messageCount := 0
 
-	for run {
-		fmt.Println("grrr")
+	for run == true {
+		select {
+		case sig := <-signals:
+			fmt.Printf("Caught signal %v: terminating\n", sig)
 
-		// commit each 10 messages
-		record, err := c.ReadMessage(-1)
-		if err == nil {
-
-			// sensorReading := &pb.SensorReading{}
-			msg := &pb.Person{}
-			// err = proto.Unmarshal(record.Value[7:], sensorReading)
-			err = proto.Unmarshal(record.Value[7:], msg)
-			if err != nil {
-				panic(fmt.Sprintf("Error deserializing the record: %s", err))
+			commitOffsets(c)
+			// os.Exit(0)
+			run = false
+		default:
+			ev := c.Poll(100)
+			if ev == nil {
+				continue
 			}
-			fmt.Printf("Message on %s: %s\n", record.TopicPartition, string(record.Value))
-			fmt.Println("seeeee: ", msg)
 
-			if count%10 == 0 {
-				log.Println("commit .......")
-				// Commit the offset asynchronously
-				go func() {
-					_, err := c.CommitMessage(record)
+			// log.Print("see the event::::: ", ev)
+			switch e := ev.(type) {
+			case *kafka.Message:
+				// fmt.Printf("Received message !!!!!!!!! : %s\n", string(e.Value))
+				record, err := c.ReadMessage(-1)
+				if err == nil {
+
+					// sensorReading := &pb.SensorReading{}
+					msg := &pb.Person{}
+					// err = proto.Unmarshal(record.Value[7:], sensorReading)
+					err = proto.Unmarshal(record.Value[7:], msg)
 					if err != nil {
-						fmt.Printf("Failed to commit offset: %s\n", err)
-						// Handle the error accordingly
+						panic(fmt.Sprintf("Error deserializing the record: %s", err))
 					}
-				}()
+
+					fmt.Println("")
+					fmt.Printf("Message on %s: %s\n", record.TopicPartition, string(record.Value))
+
+					// here we will commit "async" each 5 message
+					messageCount++
+					if messageCount%commitBatchSize == 0 {
+						go commitOffsets(c)
+					}
+
+					// go func() {
+					// 	_, err := c.CommitMessage(record)
+					// 	if err != nil {
+					// 		fmt.Printf("Failed to commit offset: %s\n", err)
+					// 		// Handle the error accordingly
+					// 	}
+					// }()
+				}
+
+			case kafka.PartitionEOF:
+				fmt.Printf("Reached end of partition %v\n", e.Partition)
+			case kafka.Error:
+				fmt.Fprintf(os.Stderr, "Error: %v\n", e)
+				commitOffsets(c)
+				run = false
 			}
-			count++
-			if count%1000000000 == 0 {
-				count = 0
-			}
 
-			// commit asynchronously at each reveived message
-			// // sensorReading := &pb.SensorReading{}
-			// msg := &pb.Person{}
-			// // err = proto.Unmarshal(record.Value[7:], sensorReading)
-			// err = proto.Unmarshal(record.Value[7:], msg)
-			// if err != nil {
-			// 	panic(fmt.Sprintf("Error deserializing the record: %s", err))
-			// }
-			// fmt.Printf("Message on %s: %s\n", record.TopicPartition, string(record.Value))
-			// fmt.Println("seeeee: ", msg)
-
-			// // Commit the offset asynchronously
-			// go func() {
-			// 	_, err := c.CommitMessage(record)
-			// 	if err != nil {
-			// 		fmt.Printf("Failed to commit offset: %s\n", err)
-			// 		// Handle the error accordingly
-			// 	}
-			// }()
-
-		} else if err.(kafka.Error).IsFatal() {
-			// fmt.Println(err)
-			log.Printf("Consumer error: %v \n", err)
-			break
-		} else {
-			// In case of any other errors(which are not Fatal),
-			// we try to commit the last offset, and we stay in the loop
-			// at the oposit of the Fatal err(previously) where we
-			// exit the loop and terminate the program
-			log.Println("See the err in the else: ", err)
-
-			wg.Add(1)
-			go handleLastcommit(c, record, &wg)
-			wg.Wait()
 		}
 	}
 
-	wg.Add(1)
-	go handleLastcommit(c, record, &wg)
-	wg.Wait()
-
-	log.Println("consumer shut down")
 }
 
-func handleLastcommit(c *kafka.Consumer, record *kafka.Message, wg *sync.WaitGroup) {
-	// this will commit in any case the last offset synchronously as we use wg
-	// no matter if we re-commit an offset as in this case kafka will ignore it
-	go func() {
-		_, err := c.CommitMessage(record)
-		if err != nil {
-			log.Printf("Failed to commit offset: %s\n", err)
-			// Handle the error accordingly
-			wg.Done()
-		}
-		log.Printf("Last offset commited ")
-		wg.Done()
-	}()
+func commitOffsets(c *kafka.Consumer) {
+	offsets, err := c.Commit()
+	if err != nil {
+		// Handle commit error
+		fmt.Println("Failed to commit offsets:", err)
+	} else {
+		// Process the committed offsets if needed
+		fmt.Println("Committed offsets:", offsets)
+	}
+}
+
+func myRebalanceCallback(c *kafka.Consumer, event kafka.Event) error {
+	switch ev := event.(type) {
+	case kafka.AssignedPartitions:
+		// Handle assigned partitions
+		fmt.Printf("Assigned partitions: %v\n", ev.Partitions)
+
+		commitOffsets(c)
+		// Start consuming from the newly assigned partitions
+
+	case kafka.RevokedPartitions:
+		// Handle revoked partitions
+		fmt.Printf("Revoked partitions: %v\n", ev.Partitions)
+
+		commitOffsets(c)
+
+		// Stop consuming from the revoked partitions and perform any necessary cleanup or checkpointing operations
+	}
+
+	// Return nil or an error, depending on your requirements
+	return nil
 }
 
 func setTopic() {
@@ -518,3 +385,223 @@ func setTopic() {
 
 	a.Close()
 }
+
+// type StickyAssignor struct {
+// 	consumer           *kafka.Consumer
+// 	assignedPartitions map[int32]bool
+// }
+//
+// func NewStickyAssignor(consumer *kafka.Consumer) *StickyAssignor {
+// 	return &StickyAssignor{
+// 		consumer:           consumer,
+// 		assignedPartitions: make(map[int32]bool),
+// 	}
+// }
+//
+// func (s *StickyAssignor) AssignPartitions(topic string, partitions []kafka.TopicPartition) {
+// 	currentAssignment, err := s.consumer.Assignment()
+// 	log.Println("See the current assignement: ", currentAssignment)
+// 	log.Println("See the stored assignement: ", s.assignedPartitions)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+//
+// 	// get existing partition
+// 	meta, err := s.consumer.GetMetadata(nil, true, 5000)
+// 	if err != nil {
+// 		fmt.Fprintf(os.Stderr, "Failed to retrieve metadata: %v\n", err)
+// 	}
+//
+// 	topicMeta, ok := meta.Topics[topic]
+// 	if !ok {
+// 		fmt.Fprintf(os.Stderr, "Topic not found in metadata: my-topic\n")
+// 	}
+//
+// 	// set the already assigned partitions
+// 	for _, partition := range topicMeta.Partitions {
+// 		s.assignedPartitions[partition.ID] = true
+// 	}
+//
+// 	// handle the partition assignment
+//
+// 	// loop on the assigned partitions then if some available assign to the next
+//
+// 	// Find the partitions that are already assigned to the consumer
+// 	// assignedPartitions := make(map[int32]bool)
+// 	// for _, p := range currentAssignment {
+// 	// 	if *p.Topic == topic {
+// 	// 		s.assignedPartitions[p.Partition] = true
+// 	// 	}
+// 	// }
+//
+// 	// // Assign the unassigned partitions
+// 	// var newAssignment []kafka.TopicPartition
+// 	// for _, p := range partitions {
+// 	// 	if !s.assignedPartitions[p.Partition] {
+// 	// 		newAssignment = append(newAssignment, p)
+// 	// 		// s.assignedPartitions[p.Partition] = true
+// 	// 	}
+// 	// }
+//
+// 	// // Manually assign the unassigned partitions
+// 	// err = s.consumer.Assign(newAssignment)
+// 	// if err != nil {
+// 	// 	panic(err)
+// 	// }
+// 	// // update the s.assignedPartitions
+// 	// for _, p := range newAssignment {
+// 	// 	s.assignedPartitions[p.Partition] = true
+// 	// }
+//
+// 	// fmt.Println("Assigned partitions:", newAssignment)
+// }
+//
+// func (s *StickyAssignor) OnRebalance(partitions []kafka.TopicPartition) error {
+// 	fmt.Printf("Rebalance event: %v\n", partitions)
+//
+// 	// Iterate through partitions and reassign the desired ones
+// 	for _, partition := range partitions {
+// 		if s.assignedPartitions[partition.Partition] {
+// 			fmt.Printf("Reassigning partition %d\n", partition.Partition)
+// 			err := s.consumer.Assign([]kafka.TopicPartition{
+// 				kafka.TopicPartition{
+// 					// Topic:     &partition.Topic,
+// 					Topic:     partition.Topic,
+// 					Partition: partition.Partition,
+// 					Offset:    kafka.OffsetInvalid,
+// 				},
+// 			})
+//
+// 			if err != nil {
+// 				fmt.Printf("Error reassigning partition %d: %v\n", partition.Partition, err)
+// 				return err
+// 			}
+// 		}
+// 	}
+//
+// 	return nil
+// }
+
+// for run {
+// 	fmt.Println("grrr")
+
+// 	// commit each 10 messages
+// 	record, err := c.ReadMessage(-1)
+// 	if err == nil {
+
+// 		// sensorReading := &pb.SensorReading{}
+// 		msg := &pb.Person{}
+// 		// err = proto.Unmarshal(record.Value[7:], sensorReading)
+// 		err = proto.Unmarshal(record.Value[7:], msg)
+// 		if err != nil {
+// 			panic(fmt.Sprintf("Error deserializing the record: %s", err))
+// 		}
+// 		fmt.Printf("Message on %s: %s\n", record.TopicPartition, string(record.Value))
+// 		fmt.Println("seeeee: ", msg)
+
+// 		if count%10 == 0 {
+// 			log.Println("commit .......")
+// 			// Commit the offset asynchronously
+// 			go func() {
+// 				_, err := c.CommitMessage(record)
+// 				if err != nil {
+// 					fmt.Printf("Failed to commit offset: %s\n", err)
+// 					// Handle the error accordingly
+// 				}
+// 			}()
+// 		}
+// 		count++
+// 		if count%1000000000 == 0 {
+// 			count = 0
+// 		}
+
+// 		// commit asynchronously at each reveived message
+// 		// // sensorReading := &pb.SensorReading{}
+// 		// msg := &pb.Person{}
+// 		// // err = proto.Unmarshal(record.Value[7:], sensorReading)
+// 		// err = proto.Unmarshal(record.Value[7:], msg)
+// 		// if err != nil {
+// 		// 	panic(fmt.Sprintf("Error deserializing the record: %s", err))
+// 		// }
+// 		// fmt.Printf("Message on %s: %s\n", record.TopicPartition, string(record.Value))
+// 		// fmt.Println("seeeee: ", msg)
+
+// 		// // Commit the offset asynchronously
+// 		// go func() {
+// 		// 	_, err := c.CommitMessage(record)
+// 		// 	if err != nil {
+// 		// 		fmt.Printf("Failed to commit offset: %s\n", err)
+// 		// 		// Handle the error accordingly
+// 		// 	}
+// 		// }()
+
+// 	} else if err.(kafka.Error).IsFatal() {
+// 		// fmt.Println(err)
+// 		log.Printf("Consumer error: %v \n", err)
+// 		break
+// 	} else {
+// 		// In case of any other errors(which are not Fatal),
+// 		// we try to commit the last offset, and we stay in the loop
+// 		// at the oposit of the Fatal err(previously) where we
+// 		// exit the loop and terminate the program
+// 		log.Println("See the err in the else: ", err)
+
+// 		wg.Add(1)
+// 		go handleLastcommit(c, record, &wg)
+// 		wg.Wait()
+// 	}
+// }
+
+// wg.Add(1)
+// go handleLastcommit(c, record, &wg)
+// wg.Wait()
+
+// log.Println("consumer shut down")
+
+// type StickyAssignor struct {
+// 	consumer *kafka.Consumer
+// }
+//
+// func (a *StickyAssignor) Assign(
+// 	partitions []kafka.TopicPartition,
+// 	members []string,
+// ) (map[string][]kafka.TopicPartition, error) {
+// 	assignments := make(map[string][]kafka.TopicPartition)
+// 	memberCount := len(members)
+//
+// 	// Assign partitions in a sticky manner
+// 	for i, partition := range partitions {
+// 		memberIndex := i % memberCount
+// 		memberID := members[memberIndex]
+// 		assignments[memberID] = append(assignments[memberID], partition)
+// 	}
+//
+// 	return assignments, nil
+// }
+//
+// func (a *StickyAssignor) OnRevocation(revokedPartitions []kafka.TopicPartition) {
+// 	// Handle logic when partitions are revoked from consumers
+// 	for _, revokedPartition := range revokedPartitions {
+// 		// Commit the current offset for revoked partitions
+// 		a.consumer.CommitOffsets([]kafka.TopicPartition{revokedPartition})
+//
+// 		// Seek to the new position for revoked partitions (end offset)
+// 		tp := kafka.TopicPartition{
+// 			Topic:     revokedPartition.Topic,
+// 			Partition: revokedPartition.Partition,
+// 			Offset:    kafka.OffsetEnd,
+// 		}
+//
+// 		_, high, err := a.consumer.QueryWatermarkOffsets(*tp.Topic, tp.Partition, 5000)
+// 		if err != nil {
+// 			// Handle error
+// 			continue
+// 		}
+//
+// 		tp.Offset = kafka.Offset(high)
+//
+// 		a.consumer.Seek(tp, 0)
+//
+// 		// Perform any other necessary actions for revoked partitions
+// 	}
+// }
